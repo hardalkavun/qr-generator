65adf502b47aa8a185751710a023d991c4e2af6d
Eren Kaptan <eren.kaptan@gazi.edu.tr>
2025-08-12 19:36:33 +0300

    Update QRGenerator.java

--- QRGenerator/src/QRGenerator.java
+++ QRGenerator/src/QRGenerator.java
@@ -1,30 +1,24 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
-import java.awt.*;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
-import com.google.zxing.WriterException;
-import com.google.zxing.client.j2se.MatrixToImageWriter;
-import com.google.zxing.common.BitMatrix;
-
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
 
 public class QRGenerator extends JFrame{
 
     public static JTextArea editTextArea = new JTextArea();     //linki aldığımız yer
 
-    
+
+
     public static void QrGenerate(String data, String filePath, int width, int height){
-        // ekle
+
     }
 
     public QRGenerator() {
 
         JFrame frame = new JFrame();
         frame.setTitle("QR Generator");
-        frame.setBounds(100, 100, 450, 300);
+        frame.setBounds(750, 340, 450, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setResizable(false);      // hocam önemli tam ekran mevzusu
@@ -40,9 +34,6 @@ public class QRGenerator extends JFrame{
         generateButton.setBounds(150, 80, 150, 40);
 
 
-     
-
-
         generateButton.addActionListener(e -> {
             String text = editTextArea.getText();
 
@@ -50,14 +41,22 @@ public class QRGenerator extends JFrame{
                 //JOptionPane test = new JOptionPane();     bu olmuyo böyle
                 //test.setMessage("hata");
                 JOptionPane.showMessageDialog(this,"Link Girin!");
+
             }
-          else {
+
+            else {
               //  String filePath = "qrcode.png"; bu ne işe yarıyo tam çözemedim        muhtemeln önce qr kodu oluşturup bir yere kaydediyo ardından ekrana gösteriyo
 
                 //generateqr code here
 
                 QrGenerate(text,"qrcode.png",5,5);
+                try {
+                    BufferedImage QrImage= ImageIO.read(new File("qrcode.png"));
 
+                } catch (IOException ex) {
+                    ex.printStackTrace();       //bunu yapınca düzeldi
+                    JOptionPane.showMessageDialog(this,"Link Girin!"+ex.getMessage());      //working
+                }
 
 
             }
@@ -65,9 +64,7 @@ public class QRGenerator extends JFrame{
         });
 
         frame.add(generateButton);
-
         frame.setVisible(true);
-
 
 
 
@@ -77,7 +74,6 @@ public class QRGenerator extends JFrame{
 
     public static void main(String[] args) {
             QRGenerator newQR = new QRGenerator();
-
 
 
 
